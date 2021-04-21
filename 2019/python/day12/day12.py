57a9d57283c8fb2fb6c96152e79ab11be06bbae3
Robin Hutmacher <robin.hutmacher@gmail.com>
2021-04-21 21:53:54 +0200

    Add Day 12 Part 2

--- 2019/python/day12/day12.py
+++ 2019/python/day12/day12.py
@@ -1,7 +1,9 @@
 from dataclasses import dataclass
 from operator import add, sub
-from itertools import combinations, starmap
+from functools import reduce
+from itertools import combinations
 import re
+import math
 
 
 # https://stackoverflow.com/a/16726462
@@ -39,7 +41,14 @@ moons = [
     Moon((6, -9, -11)),
 ]
 
-for _ in range(1000):
+
+# Split into x, y and z vectors
+initial_xs, initial_ys, initial_zs = list(zip(*[m.position for m in moons]))
+
+# Find cycle lenghts for x, y and z position independently
+cycles = {}
+i = 1
+while len(cycles) != 3:
     for moon1, moon2 in combinations(moons, 2):
         gravity = tuple(map(sign, map(sub, moon1.position, moon2.position)))
         moon1.velocity = tuple(map(sub, moon1.velocity, gravity))
@@ -48,4 +57,28 @@ for _ in range(1000):
     for moon in moons:
         moon.apply_velocity()
 
-print(sum([moon.total_energy for moon in moons]))
+    current_xs, current_ys, current_zs = list(zip(*[m.position for m in moons]))
+    x_velocity, y_velocity, z_velocity = list(zip(*[m.velocity for m in moons]))
+    if x_velocity == (0, 0, 0, 0) and current_xs == initial_xs:
+        if "x" not in cycles:
+            cycles["x"] = i
+
+    if y_velocity == (0, 0, 0, 0) and current_ys == initial_ys:
+        if "y" not in cycles:
+            cycles["y"] = i
+
+    if z_velocity == (0, 0, 0, 0) and current_zs == initial_zs:
+        if "z" not in cycles:
+            cycles["z"] = i
+
+    if i == 1000:
+        print(sum([moon.total_energy for moon in moons]))
+
+    i += 1
+
+# Calculate least common multiple
+def lcm(a, b):
+    return abs(a * b) // math.gcd(a, b)
+
+
+print(reduce(lcm, cycles.values()))
