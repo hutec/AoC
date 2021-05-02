007c226f0b1830c2ebe1f663cb407afd3a11bdf1
Robin Hutmacher <robin.hutmacher@gmail.com>
2021-05-02 10:40:38 +0200

    Add Rust Day 6

---
+++ 2019/rust/src/day6.rs
@@ -0,0 +1,31 @@
+use regex::Regex;
+use std::collections::HashMap;
+use std::fs;
+
+fn count_orbits(orbits: &HashMap<&str, &str>, orbiter: &str, acc: i32) -> i32 {
+    match orbits.get(orbiter) {
+        Some(center) => count_orbits(orbits, center, acc + 1),
+        None => acc,
+    }
+}
+
+pub fn main() {
+    let input = fs::read_to_string("inputs/day6").expect("Can't read file");
+    let lines = input.lines();
+    let re = Regex::new(r"^([\d\w]*)\)([\d\w]*)$").unwrap();
+    let mut orbits = HashMap::new();
+
+    // Build orbit hashmap
+    for line in lines {
+        let caps = re.captures(line).unwrap();
+        let center = caps.get(1).unwrap().as_str();
+        let orbiter = caps.get(2).unwrap().as_str();
+        orbits.insert(orbiter, center);
+    }
+
+    let mut count = 0;
+    for (o, _) in &orbits {
+        count += count_orbits(&orbits, o, 0);
+    }
+    println!("Result Task 1: {}", count);
+}
--- 2019/rust/src/main.rs
+++ 2019/rust/src/main.rs
@@ -1,5 +1,7 @@
 mod day1;
+mod day6;
 
 fn main() {
-    day1::main()
+    day1::main();
+    day6::main();
 }
