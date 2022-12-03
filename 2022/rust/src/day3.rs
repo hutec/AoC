e6c8c1b094c040bcef13d6b84403d28a8bddd361
Robin Hutmacher <robin.hutmacher@gmail.com>
2022-12-03 13:04:46 +0100

    Add Day 3 2022 (rust)

---
+++ 2022/rust/src/day3.rs
@@ -0,0 +1,51 @@
+use std::collections::HashSet;
+use std::fs;
+
+fn priority(item: char) -> i32 {
+    if item.is_uppercase() {
+        item as i32 - 38
+    } else {
+        item as i32 - 96
+    }
+}
+
+fn part1(lines: &Vec<&str>) {
+    let mut priority_sum = 0;
+    for line in lines.iter() {
+        // Split line in two halves
+        let (left, right) = line.split_at(line.len() / 2);
+
+        let left_set: HashSet<char> = left.chars().collect();
+        let right_set: HashSet<char> = right.chars().collect();
+
+        let intersection: Vec<char> = left_set.intersection(&right_set).cloned().collect();
+        let shared_item = intersection[0];
+        priority_sum += priority(shared_item);
+    }
+
+    println!("Priority sum: {}", priority_sum);
+}
+
+fn part2(lines: &Vec<&str>) {
+    let mut priority_sum = 0;
+    for groups in lines.chunks(3) {
+        let mut intersection: HashSet<char> = groups[0].chars().collect();
+        for line in groups.iter().skip(1) {
+            intersection = intersection
+                .intersection(&line.chars().collect())
+                .cloned()
+                .collect();
+        }
+        let shared_item = intersection.iter().next().unwrap();
+        priority_sum += priority(shared_item.clone());
+    }
+
+    println!("Priority sum: {}", priority_sum);
+}
+
+pub fn main() {
+    let lines: String = fs::read_to_string("../inputs/day03").expect("Can't read file");
+    let lines = lines.lines().collect();
+    part1(&lines);
+    part2(&lines);
+}
--- 2022/rust/src/main.rs
+++ 2022/rust/src/main.rs
@@ -1,9 +1,11 @@
 mod day1;
 mod day2_1;
 mod day2_2;
+mod day3;
 
 fn main() {
     day1::main();
     day2_1::main();
     day2_2::main();
+    day3::main();
 }
