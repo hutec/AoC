ab9b6bd99c2c0c2cf5cc75ef3f5a90e2a12002a2
Robin Hutmacher <robin.hutmacher@gmail.com>
2023-01-02 20:55:13 +0100

    Add Day 16 Part 2

--- 2022/python/day16.py
+++ 2022/python/day16.py
@@ -2,10 +2,7 @@ import re
 from typing import List, Set
 from itertools import product
 from dataclasses import dataclass
-
-with open("../inputs/day16", "r") as f:
-    seq = f.read()
-    lines = seq.split("\n")
+from itertools import chain, combinations
 
 
 @dataclass
@@ -16,15 +13,6 @@ class Valve:
 
     def __hash__(self) -> int:
         return hash(self.name)
-
-
-def get_or_create(name: str, valves: dict) -> Valve:
-    if name in valves:
-        return valves[name]
-    else:
-        valve = Valve(name, 0, [])
-        valves[name] = valve
-        return valve
 
 
 def floyd_warshall(valves: List[Valve]) -> dict:
@@ -39,69 +27,74 @@ def floyd_warshall(valves: List[Valve]) -> dict:
             dist[valve, neighbor] = 1
 
     for v1, v2, v3 in product(valves, repeat=3):
-        dist[v1, v3] = min(dist[v1, v3], dist[v1, v2] + dist[v2, v3])
+        dist[v2, v3] = min(dist[v2, v3], dist[v2, v1] + dist[v1, v3])
 
     return dist
 
 
-cache = {}
+def powerset(iterable):
+    s = list(iterable)
+    return chain.from_iterable(combinations(s, r) for r in range(len(s) + 1))
 
 
 def dfs(
     valve: Valve,
-    dist: dict,
     closed_valves: Set[Valve],
-    flow: int,
     time_left: int,
 ) -> int:
-    if time_left <= 0:
-        return flow
+    if (valve, closed_valves, time_left) in cache:
+        return cache[valve, closed_valves, time_left]
 
-    if (valve, closed_valves, flow, time_left) in cache:
-        return cache[valve, closed_valves, flow, time_left]
-
-    options = [flow * time_left]
+    maxval = 0
     for next_valve in closed_valves:
-        if next_valve.flow_rate == 0:
+        _time_left = time_left - dist[valve, next_valve] - 1
+        if _time_left <= 0:
             continue
 
-        if dist[valve, next_valve] < time_left - 1:
-            options.append(
-                flow * (dist[valve, next_valve] + 1)
-                + dfs(
-                    next_valve,
-                    dist,
-                    closed_valves - {next_valve},
-                    flow + next_valve.flow_rate,
-                    time_left - dist[valve, next_valve] - 1,
-                )
-            )
+        maxval = max(
+            maxval,
+            next_valve.flow_rate * _time_left
+            + dfs(
+                next_valve,
+                closed_valves - {next_valve},
+                _time_left,
+            ),
+        )
 
-    cache[valve, closed_valves, flow, time_left] = max(options)
-    return max(options)
+    cache[valve, closed_valves, time_left] = maxval
+    return maxval
 
 
-def main():
-    valves = {}
-    for line in lines:
-        source, *neighbors = re.findall(r"[A-Z]{2}", line)
-        flow_rate = int(re.findall(r"\d+", line)[0])
+with open("../inputs/day16", "r") as f:
+    seq = f.read()
+    lines = seq.split("\n")
 
-        source = get_or_create(source, valves)
-        neighbors = [get_or_create(neighbor, valves) for neighbor in neighbors]
-        source.flow_rate = flow_rate
-        source.neighbors = neighbors
+valves = {}
+for line in lines:
+    source, *neighbors = re.findall(r"[A-Z]{2}", line)
+    flow_rate = int(re.findall(r"\d+", line)[0])
 
-    dist = floyd_warshall(valves.values())
-    m = dfs(
-        valves["AA"],
-        dist,
-        frozenset(valves.values()),
-        0,
-        30,
-    )
-    print(m)
+    source = valves.setdefault(source, Valve(source, 0, []))
+    neighbors = [
+        valves.setdefault(neighbor, Valve(neighbor, 0, [])) for neighbor in neighbors
+    ]
+    source.flow_rate = flow_rate
+    source.neighbors = neighbors
 
+dist = floyd_warshall(valves.values())
 
-if __name__ == "__main__":
-    main()
+all_valves = frozenset(v for v in valves.values() if v.flow_rate != 0)
+cache = {}
+
+# Part 1
+m = dfs(valves["AA"], all_valves, 30)
+print(m)
+
+# Part 2
+m = 0
+for valves1 in powerset(all_valves):
+    valves1 = frozenset(valves1)
+    valves2 = all_valves - valves1
+    m = max(m, dfs(valves["AA"], valves1, 26) + dfs(valves["AA"], valves2, 26))
+
+print(m)
