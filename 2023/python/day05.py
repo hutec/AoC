56f998d9a284536cd8847d8a6ad61deb0fa9a0f0
Robin Hutmacher <robin.hutmacher@gmail.com>
2023-12-07 08:09:44 +0100

    Add day 5 part 2 2023

--- 2023/python/day05.py
+++ 2023/python/day05.py
@@ -1,13 +1,11 @@
-from typing import Optional
-from dataclasses import dataclass, field
-
+from dataclasses import dataclass
 
 with open("../inputs/05", "r") as f:
     seq = f.read()
     blocks = seq.split("\n\n")
 
 
-@dataclass
+@dataclass(frozen=True)
 class Range:
     target: int
     source: int
@@ -26,6 +24,10 @@ class Range:
         return self.target + (x - self.source)
 
     @property
+    def offset(self) -> int:
+        return self.target - self.source
+
+    @property
     def source_end(self) -> int:
         return self.source + self.length
 
@@ -33,12 +35,16 @@ class Range:
     def target_end(self) -> int:
         return self.target + self.length
 
+    def __repr__(self) -> str:
+        return (
+            f"[{self.source}, {self.source_end}] -> [{self.target}, {self.target_end}]"
+        )
+
 
 @dataclass
 class Map:
     name: str
     ranges: list[Range]
-    cache: dict[int, int] = field(default_factory=dict)
 
     @classmethod
     def from_str(cls, block):
@@ -49,16 +55,9 @@ class Map:
         return cls(name, ranges)
 
     def transform(self, x):
-        if x in self.cache:
-            return self.cache[x]
-
         for range in self.ranges:
             if range.source_contains(x):
-                r = range.transform(x)
-                self.cache[x] = r
-                return r
-
-        self.cache[x] = x
+                return range.transform(x)
         return x
 
 
@@ -73,3 +72,81 @@ for seed in seeds:
     r.append(seed)
 
 print(min(r))
+
+
+def c(r1: Range, r2: Range):
+    """Cut range 1 with range 2."""
+    result = []
+
+    # Case 1: No overlap
+    if (
+        not r1.target_contains(r2.source)
+        and not r1.target_contains(r2.source + r2.length)
+        and not r2.source_contains(r1.target)
+        and not r2.source_contains(r1.target + r1.length)
+    ):
+        return ([], [])
+
+    # Case 2: Overlap in the end
+    if r1.target_contains(r2.source) and not r1.target_contains(r2.source + r2.length):
+        offset = r2.source - r1.target
+        keep = [Range(r1.target, r1.source, offset)]
+        new = [
+            Range(
+                r2.target,
+                r1.source + offset,
+                r1.length - offset,
+            )
+        ]
+        return keep, new
+
+    # Case 3: Overlap in the beginning
+    if not r1.target_contains(r2.source) and r1.target_contains(r2.source + r2.length):
+        offset = (r2.source + r2.length) - r1.target
+        new = [Range(r2.target + (r2.length - offset), r1.source, offset)]
+        keep = [Range(r1.target + offset, r1.source + offset, r1.length - offset)]
+        return keep, new
+
+    # Case 4: r1 fully contains r2
+    if r1.target_contains(r2.source) and r1.target_contains(r2.source + r2.length):
+        offset = r2.source - r1.target
+
+        keep = [
+            Range(r1.target, r1.source, offset),
+            Range(
+                r1.target + r2.length + offset,
+                r1.source + r2.length + offset,
+                r1.length - offset - r2.length,
+            ),
+        ]
+        new = [Range(r2.target, r1.source + offset, r2.length)]
+
+        return keep, new
+
+    # Case 5: r2 fully contains r1
+    if r2.source_contains(r1.target) and r2.source_contains(r1.target + r1.length):
+        offset = r1.target - r2.source
+        return [], [Range(r2.target + offset, r1.target, r1.length)]
+
+
+seeds = {Range(s, s, l) for s, l in zip(seeds[::2], seeds[1::2])}
+
+for map in maps:
+    next = set()
+    while len(seeds) > 0:
+        seed = seeds.pop()
+        for range in map.ranges:
+            keep, new = c(seed, range)
+            if keep or new:
+                if len(keep) == 1 and keep[0] == seed:
+                    continue
+                seeds = seeds.union(keep)
+                next = next.union(new)
+                break
+        else:
+            # No matches
+            next.add(seed)
+
+    seeds = {r for r in next if r.length > 0}
+
+print(min(r.target for r in seeds if r.length > 0))
